751f0ba7b917e4081d6cb50c6dcae0e97f38ef6e
Liang Ding <d@b3log.org>
2021-05-07 09:25:01 +0800

    :bug: 剪藏插件内容带图片时剪藏失败 https://github.com/siyuan-note/insider/issues/66

--- background.js
+++ background.js
@@ -1,9 +1,21 @@
 chrome.contextMenus.create({
-    title: "Copy to SiYuan",
-    contexts: ["selection"],
-    onclick: siyuan,
+  title: "Copy to SiYuan",
+  contexts: ["selection"],
+  onclick: siyuan,
 });
 
 function siyuan(info, tab) {
-    chrome.tabs.sendMessage(tab.id, {"func": "copy"});
+  chrome.tabs.sendMessage(tab.id, {"func": "copy"});
 }
+
+chrome.webRequest.onHeadersReceived.addListener(
+  function (details) {
+    const cors = {name: "access-control-allow-origin", value: "*"};
+    const responseHeaders = details.responseHeaders.concat(cors);
+    return {responseHeaders};
+  },
+  {
+    urls: ["*://*/*"],
+  },
+  ["blocking", "responseHeaders", "extraHeaders"]
+);
