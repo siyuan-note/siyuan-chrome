2995d6f258939659746defb10bae27911d9541b9
Liang Ding <d@b3log.org>
2021-05-25 21:06:34 +0800

    :art: Clean code

--- content.js
+++ content.js
@@ -51,18 +51,18 @@ chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
 function copyToClipboard(textToCopy) {
   if (navigator.clipboard && window.isSecureContext) {
     return navigator.clipboard.writeText(textToCopy);
-  } else {
-    let textArea = document.createElement("textarea");
-    textArea.value = textToCopy;
-    textArea.style.position = "fixed";
-    textArea.style.left = "-999999px";
-    textArea.style.top = "-999999px";
-    document.body.appendChild(textArea);
-    textArea.focus();
-    textArea.select();
-    return new Promise((res, rej) => {
-      document.execCommand('copy') ? res() : rej();
-      textArea.remove();
-    });
   }
+
+  let textArea = document.createElement("textarea");
+  textArea.value = textToCopy;
+  textArea.style.position = "fixed";
+  textArea.style.left = "-999999px";
+  textArea.style.top = "-999999px";
+  document.body.appendChild(textArea);
+  textArea.focus();
+  textArea.select();
+  return new Promise((res, rej) => {
+    document.execCommand('copy') ? res() : rej();
+    textArea.remove();
+  });
 }
