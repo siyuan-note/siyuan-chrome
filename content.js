9d62a4c63e566a5739930bbb79f11afe2e47d558
Liang Ding <d@b3log.org>
2021-05-25 21:06:10 +0800

    :bug: 浏览器剪藏扩展报错 No matching signature. Fix https://github.com/siyuan-note/insider/issues/503

--- content.js
+++ content.js
@@ -41,9 +41,28 @@ chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
     if (response.code < 0) {
       alert(response.msg)
     } else {
-      navigator.clipboard.writeText(response.data.md);
+      copyToClipboard(response.data.md).catch(() => console.log('error'));
     }
   }).catch((e) => {
     console.warn('fetch post error', e)
   })
 })
+
+function copyToClipboard(textToCopy) {
+  if (navigator.clipboard && window.isSecureContext) {
+    return navigator.clipboard.writeText(textToCopy);
+  } else {
+    let textArea = document.createElement("textarea");
+    textArea.value = textToCopy;
+    textArea.style.position = "fixed";
+    textArea.style.left = "-999999px";
+    textArea.style.top = "-999999px";
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+    return new Promise((res, rej) => {
+      document.execCommand('copy') ? res() : rej();
+      textArea.remove();
+    });
+  }
+}
