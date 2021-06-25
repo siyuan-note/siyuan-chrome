dd95a6fe29b81ed18182c11239d7b694e68e5826
Vanessa <lly219@gmail.com>
2021-06-25 11:26:03 +0800

    :art: https://github.com/siyuan-note/siyuan/issues/2203

--- content.js
+++ content.js
@@ -1,3 +1,4 @@
+let messageElement = document.getElementById('siyuanmessage')
 chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
   if (request.func !== 'copy') {
     return
@@ -36,6 +37,11 @@ chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
     const image = await response.blob()
     formData.append(escape(src), image)
   }
+  if (!messageElement) {
+    document.body.insertAdjacentHTML('afterend', `<div style=" position:fixed;top: 0;z-index: 999999999;transform: translate3d(0, -100px, 0);opacity: 0;transition: opacity 0.15s cubic-bezier(0, 0, 0.2, 1) 0ms, transform 0.15s cubic-bezier(0, 0, 0.2, 1) 0ms;width: 100%;align-items: center;justify-content: center;height: 0;display: flex;" id="siyuanmessage">
+<div style="line-height: 20px;border-radius: 4px;padding: 8px 16px;color: #fff;font-size: inherit;background-color: #4285f4;box-sizing: border-box;box-shadow: 0 3px 5px -1px rgba(0, 0, 0, 0.2), 0 6px 10px 0 rgba(0, 0, 0, 0.14), 0 1px 18px 0 rgba(0, 0, 0, 0.12);transition: transform 0.15s cubic-bezier(0, 0, 0.2, 1) 0ms;transform: scale(0.8);top: 16px;position: absolute;word-break: break-word;max-width: 80vw;"></div></div>`)
+    messageElement = document.getElementById('siyuanmessage')
+  }
 
   chrome.storage.sync.get({
     ip: 'http://127.0.0.1:68061',
@@ -47,19 +53,29 @@ chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
       return response.json()
     }).then((response) => {
       if (response.code < 0) {
-        alert(response.msg)
+        showTip(response.msg)
       } else {
         copyToClipboard(response.data.md).catch(() => console.log('error'))
         if ('' !== response.msg) {
-          alert(response.msg)
+          showTip(response.msg)
         }
       }
     }).catch((e) => {
       console.warn('fetch post error', e)
     })
   })
-
 })
+
+function showTip (msg) {
+  messageElement.style.transform = 'translate3d(0, 0, 0)'
+  messageElement.style.opacity = '1'
+  messageElement.firstElementChild.innerHTML = msg
+
+  setTimeout(() => {
+    messageElement.style.transform = 'translate3d(0, -100px, 0)'
+    messageElement.style.opacity = '0'
+  }, 3000)
+}
 
 function copyToClipboard (textToCopy) {
   if (navigator.clipboard && window.isSecureContext) {
