ed469a8e78dc859ec73a25a9686a08465c7756ac
Liang Ding <d@b3log.org>
2022-01-07 17:57:21 +0800

    :art: 剪藏扩展时间格式改为 `yyyy-MM-dd HH:mm:ss` https://github.com/siyuan-note/siyuan/issues/3779

--- background.js
+++ background.js
@@ -139,28 +139,27 @@ chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
 })
 
 function getDateTime() {
-  var now = new Date();
-  var year = now.getFullYear();
-  var month = now.getMonth() + 1;
-  var day = now.getDate();
-  var hour = now.getHours();
-  var minute = now.getMinutes();
-  var second = now.getSeconds();
-  if (month.toString().length == 1) {
+  const now = new Date();
+  const year = now.getFullYear();
+  let month = now.getMonth() + 1;
+  let day = now.getDate();
+  let hour = now.getHours();
+  let minute = now.getMinutes();
+  let second = now.getSeconds();
+  if (month.toString().length === 1) {
     month = '0' + month;
   }
-  if (day.toString().length == 1) {
+  if (day.toString().length === 1) {
     day = '0' + day;
   }
-  if (hour.toString().length == 1) {
+  if (hour.toString().length === 1) {
     hour = '0' + hour;
   }
-  if (minute.toString().length == 1) {
+  if (minute.toString().length === 1) {
     minute = '0' + minute;
   }
-  if (second.toString().length == 1) {
+  if (second.toString().length === 1) {
     second = '0' + second;
   }
-  var dateTime = year + '/' + month + '/' + day + ' ' + hour + ':' + minute + ':' + second;
-  return dateTime;
+  return year + '-' + month + '-' + day + ' ' + hour + ':' + minute + ':' + second;
 }
